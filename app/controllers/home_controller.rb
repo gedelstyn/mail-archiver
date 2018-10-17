fa6ac3d0ad748bc367e22c7fa141b0de05039174
ddregalo <ddregalo@gmail.com>
2018-10-17 20:49:24 +0100

    New user + create methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,21 @@
 class HomeController < ApplicationController
 
-    def index
-        @users = User.all
-    end
+  def index
+      @users = User.all
+  end
+
+  def new_user
+    @new_user = User.new
+  end
+  
+  def create
+    @new_user = User.create(new_user_params)
+    redirects_to home_path
+  end
+
+  private
+  def new_user_params
+    params.require(:new_user).permit(:first_name, :last_name, :email)
+  end
+    
 end
