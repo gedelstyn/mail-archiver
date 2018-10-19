058c786897c11f9ba458fdfb1b5173c9abcce3d1
gedelstyn <guy@h21.uk>
2018-10-19 18:19:01 +0100

    Stop new users being created by omniauth, added can_login logic

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,13 +3,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     auth = request.env["omniauth.auth"]
-    user = User.where(email: auth["info"]["email"])
-            .first_or_initialize(email: auth["info"]["email"], password: Devise.friendly_token[0,20])
-    user.save!
-
+    user = User.where(email: auth["info"]["email"], can_login: true).first # Got rid of this section, it creates the user if they dont exist#_or_initialize(email: auth["info"]["email"], password: Devise.friendly_token[0,20])
+    #user.save!
+    if !user
+      raise "Sorry, your account is not registered, please contact your administrator"
+    end
     user.remember_me = false
     sign_in(:user, user)
-
     redirect_to after_sign_in_path_for(user)
   end
 end
