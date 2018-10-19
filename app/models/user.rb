5b2c454a59cd32ae33ca6b920c87c6294490616c
gedelstyn <guy@h21.uk>
2018-10-19 16:28:04 +0100

    allow user to login with email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,37 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, :omniauth_providers => [:google_oauth2]
 
+  # def self.from_omniauth(auth)
+  #   # Either create a User record or update it based on the provider (Google) and the UID   
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.token = auth.credentials.token
+  #     user.expires = auth.credentials.expires
+  #     user.expires_at = auth.credentials.expires_at
+  #     user.refresh_token = auth.credentials.refresh_token
+  #   end
+  # end
+
+
   def self.from_omniauth(auth)
-    # Either create a User record or update it based on the provider (Google) and the UID   
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.token = auth.credentials.token
-      user.expires = auth.credentials.expires
-      user.expires_at = auth.credentials.expires_at
-      user.refresh_token = auth.credentials.refresh_token
+    user = User.find_by(email: auth.info.email)
+    if user
+      user.provider = auth.provider
+      user.uid = auth.uid
+      return user
+    end
+  
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.skip_confirmation! 
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
     end
   end
+
+
+
+
+
 end
