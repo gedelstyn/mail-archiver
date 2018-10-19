e750e9a0a8630fabee42eb1cb3656d28ccf3b372
gedelstyn <guy@h21.uk>
2018-10-19 16:27:16 +0100

    Allow users to authenticate with e-mail rather than UID and Provider

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,14 +1,13 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # replace with your authenticate method
   skip_before_action :authenticate_user!
 
   def google_oauth2
     auth = request.env["omniauth.auth"]
-    user = User.where(provider: auth["provider"], uid: auth["uid"])
+    user = User.where(email: auth["info"]["email"])
             .first_or_initialize(email: auth["info"]["email"], password: Devise.friendly_token[0,20])
     user.save!
 
-    user.remember_me = true
+    user.remember_me = false
     sign_in(:user, user)
 
     redirect_to after_sign_in_path_for(user)
