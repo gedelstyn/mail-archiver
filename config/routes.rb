c5d4b5b5ef0c6ef4eba09ed4bb05bd68b0025d25
gedelstyn <guy@h21.uk>
2018-10-19 16:29:50 +0100

    add route to allow homepage to talk to users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :user, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+  resources :users
   root 'home#index'
-  get 'home/new_user'
+  get 'users/new'
+  get 'users/index'
+  get 'users/edit'
+  get 'users/show'
+  get "home", to: 'users#new'
+
+
 end
