7ba7762619b5638b23f06eeabe9bfabbd950de65
gedelstyn <guy@h21.uk>
2018-10-25 23:27:32 +0100

    Added the start backup method

--- app/controllers/backup_controller.rb
+++ app/controllers/backup_controller.rb
@@ -1,2 +1,18 @@
 class BackupController < ApplicationController
+    def start_backup
+        options = {command: "backup"}
+
+        begin
+            configuration = Imap::Backup::Configuration::List.new(options[:accounts])
+        rescue Imap::Backup::ConfigurationNotFound
+            Imap::Backup::Configuration::Setup.new.run
+            exit
+        end
+        
+        configuration.setup_logging
+        
+        configuration.each_connection do |connection|
+            connection.run_backup
+        end
+    end
 end
