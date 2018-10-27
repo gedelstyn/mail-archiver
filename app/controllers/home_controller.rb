0d8e5d781e88721f314eb755048c90bfe7d0d289
gedelstyn <guy@h21.uk>
2018-10-27 15:23:18 +0100

    add start backup method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,6 +16,23 @@ class HomeController < ApplicationController
     redirects_to home_path
   end
 
+  def start_backup
+    options = {command: "backup"}
+
+    begin
+        configuration = Imap::Backup::Configuration::List.new(options[:accounts])
+    rescue Imap::Backup::ConfigurationNotFound
+        Imap::Backup::Configuration::Setup.new.run
+        exit
+    end
+    
+    configuration.setup_logging
+    
+    configuration.each_connection do |connection|
+        connection.run_backup
+    end
+  end
+
   private
   def new_user_params
     params.require(:new_user).permit(:first_name, :last_name, :email)
